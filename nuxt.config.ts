9c5204bd9983ca3099dcb15f924d98d4a7b0e32b
yhonda-ohishi <m.tama.ramu@gmail.com>
2025-01-09 06:09:04 +0000

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   // ssr: false,
+  nitro:{
+    preset:"cloudflare-pages"
+  },
   devServer: {
     host: '0.0.0.0',
     // https: {
